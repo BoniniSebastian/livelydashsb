c565786b9a8b86ea122da7aa67ba298c965967a5
BoniniSebastian <sebastian.bonini@gmail.com>
2026-04-01 16:36:55 +0200

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "DIN_API_KEY",
-  authDomain: "DIN_DOMAIN",
+  apiKey: "AIzaSyBs_SSS3bh1L-gK90svMRddW0wWKfAUQ5c",
+  authDomain: "gamevibe-88e75.firebaseapp.com",
   projectId: "gamevibe-88e75",
-  storageBucket: "DIN_BUCKET",
-  messagingSenderId: "DIN_ID",
-  appId: "DIN_APP_ID"
+  storageBucket: "gamevibe-88e75.firebasestorage.app",
+  messagingSenderId: "288622618498",
+  appId: "1:288622618498:web:3809cec91af8981157d19b"
 };
 
 const app = initializeApp(firebaseConfig);
